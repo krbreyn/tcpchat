0aa7d3d16842eb01292787f0084f1ae6706dd36c
krbreyn <krbreyn@gmail.com>
2025-01-29 23:33:55 -0500

    basic, mostly copied, should work, to be expanded

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/krbreyn/tcpchat
 
 go 1.23.5
+
+require github.com/krbreyn/must v0.0.0-20250130040314-0dde2b523bd2 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,75 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/krbreyn/must"
+)
+
+// todo - grab and print local ip on startup so you know what to connect to. also say 'localhost' for clarity
+
+func main() {
+	// server vars
+	numClients := 0
+	clients := make(map[net.Conn]int) // int is client id
+	newClients := make(chan net.Conn)
+	deadClients := make(chan net.Conn)
+	msgs := make(chan string)
+
+	server := must.Must1(net.Listen("tcp", ":23"))
+
+	// accept connections
+	go func() {
+		for {
+			// TODO just handle and print to console
+			conn := must.Must1(server.Accept())
+			newClients <- conn
+		}
+	}()
+
+	// main loop
+	for {
+		select {
+		// new clients
+		case conn := <-newClients:
+			log.Printf("accepted %d", numClients)
+			clients[conn] = numClients
+			numClients++
+
+			// read messages from client
+			go func(conn net.Conn, id int) {
+				reader := bufio.NewReader(conn)
+				for {
+					msg, err := reader.ReadString('\n')
+					if err != nil {
+						break
+					}
+					msgs <- fmt.Sprintf("client %d > %s", id, msg)
+				}
+				// if broken, client is dead
+				deadClients <- conn
+			}(conn, clients[conn])
+
+		// dead clients
+		case conn := <-deadClients:
+			log.Printf("disconnected %v", clients[conn])
+			delete(clients, conn)
+
+		// handle messages
+		case msg := <-msgs:
+			for conn := range clients {
+				go func(conn net.Conn, msg string) {
+					_, err := conn.Write([]byte(msg))
+					if err != nil {
+						deadClients <- conn
+					}
+				}(conn, msg)
+			}
+			log.Printf("msg: %s", msg)
+			log.Printf("%d clients connected", len(clients))
+		}
+	}
+}
